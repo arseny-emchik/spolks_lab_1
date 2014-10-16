229800c38a8112c9e5a99398b8c34043393c6a75
ars <arsemchik@gmail.com>
2014-10-16 17:26:01 +0300

    rewrite code

--- server.rb
+++ server.rb
@@ -3,9 +3,30 @@
 require 'socket'
 require 'colored'
 
+module Connection
+  class SocketTCP < Socket
+    def initialize(port, addr)
+      super(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+      setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR, true)
+      @sockaddr = Socket.sockaddr_in(port, addr)
+    end
+
+    def sock_bind(queue = 10)
+      bind(@sockaddr)
+      listen(queue)
+    end
+  end
+end
+
+
+
 class Server
+  ADDR = '192.168.43.195'.freeze
+
   def initialize(port)
-    @server = TCPServer.new port
+    # @server = TCPServer.new port
+    @server = Connection::SocketTCP.new(port, ADDR)
+    @server.sock_bind
     puts "Server is running on: ".red + "#{port}".green + " port.".red
   end
 
@@ -18,7 +39,8 @@ class Server
   private
 
   def client_listener
-    Thread.start(@server.accept) do |client, _|
+    Thread.start(@server.accept) do |client, client_info|
+      p client_info
       loop do
         data_str = client.gets.chomp
 
@@ -33,5 +55,5 @@ class Server
   end
 end
 
-server = Server.new(ARGV[0].nil? ? 2000 : ARGV[0].to_i)
+server = Server.new(ARGV[0].nil? ? 2004 : ARGV[0].to_i)
 server.run
